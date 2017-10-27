a0bf41829b478e879e3c2925e1bff494873af66e
Paul Lawrence <paul@thelawrences.org>
2017-10-27 20:39:24 +0000

    Added code to control a 7-segment display

--- hello.c
+++ hello.c
@@ -4,15 +4,29 @@
 
 #include <util/delay.h>
 
+static const int digits[] = {
+  0b11010111,
+  0b00000011,
+  0b11100110,
+  0b10100111,
+  0b00110011,
+  0b10110101,
+  0b11110101,
+  0b00000111,
+  0b11110111,
+  0b00110111,
+};
+
 int main(int argc, char** argv)
 {
+	int i;
 	DDRB = 0xff;
 
 	for(;;) {
-		PORTB = 0xff;
-		_delay_ms(1000);
-		PORTB = 0x00;
-		_delay_ms(1000);
+ 		for (i = 0; i < sizeof(digits)/sizeof(*digits); ++i) {
+			PORTB = digits[i];
+			_delay_ms(500);
+		}
 	}
 	return 0;
 }
