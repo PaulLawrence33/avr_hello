072425e372b844772bbdec481d08bb83f35a1237
Paul Lawrence <paul@thelawrences.org>
2017-10-30 14:55:27 +0000

    Counts to 100 using two segments

--- hello.c
+++ hello.c
@@ -2,30 +2,53 @@
 #include <avr/io.h>
 #include <avr/sleep.h>
 
+// a
+//f b
+// g
+//e c
+// d .
+
 static const int digits[] = {
-  0b11010111,
-  0b00000011,
-  0b11100110,
-  0b10100111,
-  0b00110011,
-  0b10110101,
-  0b11110101,
+  //.gfedcba 
+  0b00111111,
+  0b00000110,
+  0b01011011,
+  0b01001111,
+  0b01100110,
+  0b01101101,
+  0b01111101,
   0b00000111,
-  0b11110111,
-  0b00110111,
+  0b01111111,
+  0b01100111,
 };
+
+const int displays = 2;
 
 ISR(TIMER2_COMPA_vect, ISR_BLOCK)
 {
 	static int seconds = 0;
 	static int one_twenty_fifths = 0;
+	static int digit = 0;
+	static char segments[2];
+
+	digit = (digit + 1) % displays;
+	PORTC |= 0x0f;
+	PORTB = segments[digit];
+	PORTC &= 0xff ^ (1 << digit); 
+	
+	if (one_twenty_fifths == 62) {
+		segments[1] &= 0x7f;
+		segments[0] |= 0x80;
+	}
 
 	if(++one_twenty_fifths != 125)
 		return;
 
 	one_twenty_fifths = 0;
+
 	++seconds;
-	PORTB = digits[seconds % (sizeof(digits)/sizeof(*digits))];
+	segments[1] = digits[seconds % 10] | 0x80;
+	segments[0] = digits[seconds / 10 % 10];
 }
 
 // initialize timer, interrupt and variable
@@ -51,6 +74,9 @@ int main(int argc, char** argv)
 {
 	timer2_init();
 	DDRB = 0xff;
+	DDRC = 0xff;
+
+	PORTC = 0x00;
 
 	sei();
 	set_sleep_mode(SLEEP_MODE_PWR_SAVE);
